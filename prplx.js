23e9ca01215cface22d0512bb189d6feadf5d755
jollygiraffe <61397349+jollygiraffe@users.noreply.github.com>
2025-07-03 14:43:09 -0700

    Update prplx.js

--- prplx.js
+++ prplx.js
@@ -2,41 +2,20 @@ let obj = {
   request_date_ms: Date.now(),
   request_date: new Date().toISOString(),
   subscriber: {
-    non_subscriptions: {},
-    first_seen: "2025-06-02T07:52:55Z",
-    original_application_version: "62",
-    other_purchases: {},
-    management_url: null,
-    subscriptions: {
-      "7S8W4W365S.ai.perplexity.mac": {
-        ownership_type: "PURCHASED",
-        store: "app_store",
-        is_sandbox: false,
+    entitlements: {
+      "Perplexity Pro": {
         expires_date: "2099-12-31T23:59:59Z",
-        original_purchase_date: "2025-06-02T07:52:55Z",
+        product_identifier: "7S8W4W365S.ai.perplexity.mac",
         purchase_date: "2025-06-02T07:52:55Z"
       }
     },
-    entitlements: {
-      "Perplexity Pro": {
-        ownership_type: "PURCHASED",
-        store: "app_store",
-        is_sandbox: false,
+    subscriptions: {
+      "7S8W4W365S.ai.perplexity.mac": {
         expires_date: "2099-12-31T23:59:59Z",
-        original_purchase_date: "2025-06-02T07:52:55Z",
-        purchase_date: "2025-06-02T07:52:55Z",
-        product_identifier: "7S8W4W365S.ai.perplexity.mac"
+        purchase_date: "2025-06-02T07:52:55Z"
       }
-    },
-    original_purchase_date: "2025-06-02T07:52:55Z",
-    original_app_user_id: "$RCAnonymousID:06923c3f1dfc4f23b1b56c9e24dbdffc",
-    last_seen: new Date().toISOString()
-  },
-  offerings: [],
-  current_offering_id: "pro_2023-09-22",
-  subscription_source: "PRO",
-  subscription_status: "PRO"
+    }
+  }
 };
 
-$done({ body: '{"test":"ok"}' });
-
+$done({ body: JSON.stringify(obj) });
